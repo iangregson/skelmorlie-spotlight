8a4675c88e0f54051f69d76223645d87af49dcf1
Ian Gregson <ian@gregson.me>
2016-11-16 17:10:39 +0000

    added antimations

--- src/frontend/app/components/ArticlesView/ArticlesView.js
+++ src/frontend/app/components/ArticlesView/ArticlesView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import FlipMove from 'react-flip-move'
 
 import './ArticlesView.css'
 
@@ -14,7 +15,9 @@ export class ArticlesView extends React.Component {
   render() {
     return (
       <div className="container">
-        { this.store.articles.map(article => <ArticleEntry key={ article.id } article={ article } />) }
+        <FlipMove enterAnimation="elevator" leaveAnimation="elevator">
+          { this.store.articles.map(article => <ArticleEntry key={ article.id } article={ article } />) }
+        </FlipMove>
       </div>
     )
   }
